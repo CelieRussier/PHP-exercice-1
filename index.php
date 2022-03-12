248010624b1d61633e549afa10b3ad1c82a08836
CelieRussier <91675494+CelieRussier@users.noreply.github.com>
2022-03-12 23:53:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$moviesName = "Indiana Jones and the Last Crusade";
+$watchedMovie = "False";
+$releaseDate = 1989;
+$moviesScore = 8.2;
+echo $moviesName, "    ", $watchedMovie, "     ", $releaseDate, "     ", $moviesScore;
+?>
